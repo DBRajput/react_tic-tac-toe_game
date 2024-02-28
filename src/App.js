4370ec2f515b4b966a5ed5afb009afba9dc31ce7
DBRajput <itsdigvijaysing@gmail.com>
2024-02-29 01:15:04 +0530

    One way to tackle the solution but it's wrong

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './tic-tac-toe.png';
 import './App.css';
 import { useState } from 'react';
+
+function Player(){
+  let currentVal=0;
+  currentVal=currentVal+2;
+  console.log(currentVal);
+  let sym="X";
+  if(currentVal/2==1){
+    sym="O";
+  }
+  return sym; 
+}
 
 function Square({value}) {
   const[val,setVal] = useState(null);
   function handleClick() {
+    value=Player();
     setVal(value);
+    console.log('### Player: '+value);
   }
   return (
     <>
@@ -17,17 +29,17 @@ function Square({value}) {
 
 function Board() {
   return <>
-    <div className='Board'>
+    <div className='Board' style={{display:"flex"}}>
         <Square value='1'/>
         <Square value='2'/>
         <Square value='3'/>
     </div>
-    <div className='Board'>
+    <div className='Board' style={{display:"flex"}}>
         <Square value='4'/>
         <Square value='5'/>
         <Square value='6'/>
     </div>
-    <div className='Board'>
+    <div className='Board' style={{display:"flex"}}>
         <Square value='7'/>
         <Square value='8'/>
         <Square value='9'/>
@@ -39,7 +51,7 @@ function App() {
   return (
     <div className="App" style={{backgroundColor:'#282c34'}}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" style={{height:'150px', width:'150px'}} />
+        <img src="/tic-tac-toe.png" className="App-logo" alt="logo" style={{height:'150px', width:'150px'}} />
         <h3>React Tic-Tac-Toe Game</h3>
       </header>
       <div className="Game-Board">

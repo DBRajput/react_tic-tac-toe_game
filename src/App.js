a0f0a3846c47649d31800ad1783c09548b47dfdc
DBRajput <itsdigvijaysing@gmail.com>
2024-02-29 03:55:17 +0530

    code completed, with some updates

--- src/App.js
+++ src/App.js
@@ -1,48 +1,83 @@
 import './App.css';
 import { useState } from 'react';
 
-function Player(){
-  let currentVal=0;
-  currentVal=currentVal+2;
-  console.log(currentVal);
-  let sym="X";
-  if(currentVal/2==1){
-    sym="O";
-  }
-  return sym; 
+// function which will print on screen when someone wins
+function Winner(props) {
+  console.log('Winner Rendered'+props.winner);
+  return (
+    <div>
+      <h2 style={{color:"white"}}>Winner is {props.winner}</h2>
+    </div>
+  );  
 }
 
-function Square({value}) {
-  const[val,setVal] = useState(null);
-  function handleClick() {
-    value=Player();
-    setVal(value);
-    console.log('### Player: '+value);
+// function calculate winner
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2], [3,4,5], [6,7,8], // Horizontal
+    [0,3,6], [1,4,7], [2,5,8], // Vertical
+    [0,4,8], [2,4,6] // Diagonal
+  ];
+  for (let i=0; i<lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]) {
+      Winner({winner:squares[a]});
+      return squares[a];
+    }
   }
+  return null;
+}
+
+
+function Square({value, onSquareClick}) {
+  // console.log('Square Rendered'+value);
   return (
     <>
-      {/* <button style={{height: '60px', width : '60px'}} onClick={handleClick}>{val}</button> */}
-      <button className='btn' onClick={handleClick}>{val}</button>
+      <button className='btn' onClick={onSquareClick}>{value}</button>
     </>
   )
 }
 
 function Board() {
+  const[squares, setSquares] = useState(Array(9).fill(null));
+  const[xIsNext, setXIsNext] = useState(true);
+  const[win, setWin] = useState("~");
+  
+  function handleClick(index) {
+    const nextSquares = squares.slice(); //Slice is used to create Exact array copy of squares.
+    if(squares[index]==null && calculateWinner(squares)==null){
+      if (xIsNext) {
+        nextSquares[index] = "X";
+        setXIsNext(false);
+      } else {
+        nextSquares[index] = "O";
+        setXIsNext(true);
+      }
+      setSquares(nextSquares);
+    }else if(calculateWinner(squares)!=null){
+      alert('Winner is '+calculateWinner(squares));
+      setWin(calculateWinner(squares));
+    }
+  }
+
   return <>
     <div className='Board' style={{display:"flex"}}>
-        <Square value='1'/>
-        <Square value='2'/>
-        <Square value='3'/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
     </div>
     <div className='Board' style={{display:"flex"}}>
-        <Square value='4'/>
-        <Square value='5'/>
-        <Square value='6'/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
     </div>
     <div className='Board' style={{display:"flex"}}>
-        <Square value='7'/>
-        <Square value='8'/>
-        <Square value='9'/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+    </div>
+    <div>
+      <Winner winner={win} />
     </div>
   </>
 }
